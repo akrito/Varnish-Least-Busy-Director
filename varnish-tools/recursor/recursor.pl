aa8a06081fb63ee12628c427d75f41359cfebd40
des <des@d4fa192b-c00b-0410-8231-f00ffab90ce4>
2006-07-05 10:39:09 +0000

    Increase the period to 100000 documents Send CRLF instead of just LF Vary expiry from 300 to 1800 seconds Tack on 8 kB of blank lines
    git-svn-id: http://varnish.projects.linpro.no/svn/trunk@319 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- varnish-tools/recursor/recursor.pl
+++ varnish-tools/recursor/recursor.pl
@@ -9,11 +9,13 @@ use CGI;
 my $q = new CGI;
 
 my $foo = int($q->param('foo'));
-my $i = ($foo * 2) % 5000;
-my $j = ($foo * 2 + 1) % 5000;
+my $i = ($foo * 2) % 100000;
+my $j = ($foo * 2 + 1) % 100000;
 
-print $q->header(-expires=>'+60m');
+my $exp = ($foo % 1500) + 300;	# 300 to 1800 seconds
+print $q->header(-expires=>"+$exp");
 
-print "<h1>Page $foo</h1>\n";
-print "<p><a href=\"/cgi-bin/recursor.pl?foo=$i\">Link $i</a></p>\n";
-print "<p><a href=\"/cgi-bin/recursor.pl?foo=$j\">Link $j</a></p>\n";
+print "<h1>Page $foo</h1>\r\n";
+print "<p><a href=\"/cgi-bin/recursor.pl?foo=$i\">Link $i</a></p>\r\n";
+print "<p><a href=\"/cgi-bin/recursor.pl?foo=$j\">Link $j</a></p>\r\n";
+print "\r\n"x4096
