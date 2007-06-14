28caa7cc8688687cb88e1e9038861943ab36f010
des <des@d4fa192b-c00b-0410-8231-f00ffab90ce4>
2007-06-14 14:12:32 +0000

    Add a simple test case for Vary: handling.
    git-svn-id: http://varnish.projects.linpro.no/svn/trunk@1514 d4fa192b-c00b-0410-8231-f00ffab90ce4

---
+++ varnish-tools/regress/lib/Varnish/Test/Case/Vary.pm
@@ -0,0 +1,85 @@
+#!/usr/bin/perl -Tw
+#-
+# Copyright (c) 2006 Linpro AS
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer
+#    in this position and unchanged.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# $Id$
+#
+
+package Varnish::Test::Case::Vary;
+
+use strict;
+use base 'Varnish::Test::Case';
+
+our %languages = (
+    'en' => "Hello World!\n",
+    'no' => "Hallo Verden!\n",
+);
+
+sub testVary($) {
+    my ($self) = @_;
+
+    my $client = $self->new_client;
+    my $request = HTTP::Request->new('GET', '/');
+
+    foreach my $lang (keys %languages) {
+	$request->header('Accept-Language', $lang);
+	$request->protocol('HTTP/1.1');
+	$client->send_request($request, 2);
+	my $response = $self->run_loop;
+	die 'No (complete) response received'
+	    unless defined($response);
+	die 'Empty body'
+	    if $response->content eq '';
+	die 'Incorrect body'
+	    if $response->content ne $languages{$lang};
+    }
+}
+
+sub ev_server_request($$$$) {
+    my ($self, $server, $connection, $request) = @_;
+
+    my $body;
+    my @headers;
+    if (my $lang = $request->header("Accept-Language")) {
+	$lang = 'en'
+	    unless ($lang && $languages{$lang});
+	$body = $languages{$lang};
+	push(@headers, ('Language', $lang));
+    } else {
+	die 'Not ready for this!';
+    }
+
+    my $response = HTTP::Response->new(200, undef,
+				       [ 'Connection', 'close',
+					 'Content-Length', length($body),
+					 'Vary', 'Accept-Language',
+					 @headers ],
+				       $body);
+    $response->protocol('HTTP/1.1');
+    $connection->send_response($response);
+}
+
+1;
