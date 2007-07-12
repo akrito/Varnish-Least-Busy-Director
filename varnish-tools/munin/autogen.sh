b6cc6f470bf857b169386c881892025cd8db2680
des <des@d4fa192b-c00b-0410-8231-f00ffab90ce4>
2007-07-12 22:38:24 +0000

    I got bored and wrote an all-singing, all-dancing Munin plugin to replace the ones contributed by Anders and Bjørn.
    git-svn-id: http://varnish.projects.linpro.no/svn/trunk@1679 d4fa192b-c00b-0410-8231-f00ffab90ce4

---
+++ varnish-tools/munin/autogen.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# $Id$
+#
+
+if [ -d /usr/local/gnu-autotools/bin ] ; then
+	PATH=/usr/local/gnu-autotools/bin:${PATH}
+	export PATH
+	FIX_BROKEN_FREEBSD_PORTS="-I /usr/local/share/aclocal"
+fi
+
+automake_version=$(automake --version | tr ' ' '\n' | egrep '^[0-9]\.[0-9a-z.-]+')
+if [ -z "$automake_version" ] ; then
+    echo "unable to determine automake version"
+    exit 1
+else
+    case $automake_version in
+	0.*|1.[0-8]|1.[0-8][.-]*)
+	    echo "your version of automake ($automake_version) is too old;" \
+		"you need 1.9 or newer."
+	    exit 1
+	    ;;
+	*)
+	    ;;
+    esac
+fi
+
+set -ex
+
+aclocal ${FIX_BROKEN_FREEBSD_PORTS}
+automake --add-missing --copy --foreign
+autoconf
