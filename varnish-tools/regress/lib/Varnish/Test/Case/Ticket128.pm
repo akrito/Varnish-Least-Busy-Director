d6ffd76a13d19e18ae220bff6558ec9006c412e6
des <des@d4fa192b-c00b-0410-8231-f00ffab90ce4>
2007-07-24 13:51:29 +0000

    Use a regexp search instead of index()
    git-svn-id: http://varnish.projects.linpro.no/svn/trunk@1747 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- varnish-tools/regress/lib/Varnish/Test/Case/Ticket128.pm
+++ varnish-tools/regress/lib/Varnish/Test/Case/Ticket128.pm
@@ -57,7 +57,7 @@ sub testSyntheticError($) {
     die "Incorrect response code\n"
 	if $response->code != $CODE;
     die "Incorrect response message\n"
-	unless index($response->content, $MESSAGE);
+	unless $response->content =~ m/\Q$MESSAGE\E/o;
 
     $client->shutdown();
 
